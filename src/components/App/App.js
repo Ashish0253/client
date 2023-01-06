b182abf9df76c1771bfa827c5bb230623bf2959e
Ashish0253 <ashishrana0253@gmail.com>
2023-01-06 23:14:21 +0530

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
+import "./App.css";
+
+export default function App() {
+  const [returnedBusinesses, setReturnedBusinesses] = useState([]);
+  const [click, setClick] = useState(0);
+
+  async function searchYelp(term, location, sortBy) {
+    var data = { term, location, sortBy };
+
+    const local = "http://localhost:9000/.netlify/functions/server";
+    const host = "https://ashishkaserver.netlify.app/.netlify/functions/server";
+
+    await fetch(local + "/yelp", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setReturnedBusinesses(data.body);
+        console.log(data);
+      });
+  }
+
+  return (
+    <div className="App">
+      <h1>Dinerley</h1>
+      <SearchBar searchYelp={searchYelp} setClick={setClick} click={click} />
+      <BusinessList businesses={returnedBusinesses} />
+    </div>
+  );
+}
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import "./SearchBar.css";
+
+export default function SearchBar(props) {
+  const [term, setTerm] = useState("");
+  const [location, setLocation] = useState("");
+  const [sortBy, setSortBy] = useState("best_match");
+
+  const sortByOptions = {
+    "Best Match": "best_match",
+    "Highest Rated": "rating",
+    "Most Reviewed": "review_count",
+  };
+
+  function getSortByClass(sortByOption) {
+    return sortBy === sortByOption ? "active" : "";
+  }
+
+  function handleSortByChange(sortByOption) {
+    setSortBy(sortByOption);
+  }
+
+  function handleTermChange(event) {
+    setTerm(event.target.value);
+  }
+
+  function handleLocationChange(event) {
+    setLocation(event.target.value);
+  }
+
+  function handleSearch(event) {
+    props.searchYelp(term, location, sortBy);
+
+    event.preventDefault();
+  }
+
+  function RenderSortByOptions() {
+    return Object.keys(sortByOptions).map((sortByOption) => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+
+      return (
+        <li
+          key={sortByOptionValue}
+          className={getSortByClass(sortByOptionValue)}
+          // onClick={handleSortByChange(sortByOptionValue)}
+        >
+          {sortByOption}
+        </li>
+      );
+    });
+  }
+
+  return (
+    <div className="SearchBar">
+      <div className="SearchBar-sort-options">
+        <React.StrictMode>
+          <ul>
+            <RenderSortByOptions />
+          </ul>
+        </React.StrictMode>
+      </div>
+      <div className="SearchBar-fields">
+        <input onChange={handleTermChange} placeholder="Search Businesses" />
+        <input onChange={handleLocationChange} placeholder="Where?" />
+      </div>
+      <div className="SearchBar-submit">
+        <a href="/#" onClick={handleSearch}>
+          Search
+        </a>
+      </div>
+    </div>
+  );
+}
